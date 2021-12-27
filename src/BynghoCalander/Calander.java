70df00cfbb46c710e89c2adf55326fc5135f6436
chobh <chobh@DESKTOP-3GAO4TA>
2021-12-27 15:20:13 +0900

    method update

--- src/BynghoCalander/Calander.java
+++ src/BynghoCalander/Calander.java
@@ -2,25 +2,35 @@ package BynghoCalander;
 
 import java.util.Scanner;
 
+
 public class Calander {
 
-	public static void main(String[] argc) {
-
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
+	
+	public static int getmaxDaysofMonth(int nMonth) {
+		
+		return MAX_DAYS[nMonth-1];
+	}
+	
+	public static void printDays() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+	}
+	public static void main(String[] argc) {
 
 		// 숫자를 입력 받아 최대 일수를 출력하는 프로그램
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println(" 달을 입력 하세요");
 		int nmonth = sc.nextInt();
+	
 		
-		int[] nMaxdays = {31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
+		System.out.printf("%d월은 %d일 까지 있습니다. \n", nmonth,getmaxDaysofMonth(nmonth));
 		
-		System.out.printf("%d월은 %d일 까지 있습니다.", nmonth,nMaxdays[nmonth-1]);
+		printDays();
 	}
 }
